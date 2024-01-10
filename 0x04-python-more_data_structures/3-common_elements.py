b0bddc0f3d4fb56dc27ece89f633aed8ee03bad6
ezekielgwamna <izzymarc@gmail.com>
2024-01-10 15:49:39 -0800

    alternate common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
-
-
 def common_elements(set_1, set_2):
     """
-    A function that returns a set of
-    common elements in two sets
+    A function that returns a set of common elements in two sets
     """
-    return(set_1 & set_2)
+    return {element for element in set_1 if element in set_2}
